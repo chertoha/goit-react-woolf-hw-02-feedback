5881bf5f1a2a1e92b12723aa0452eaacc96992df
Anton <a.chertok@akatech.com.ua>
2024-02-01 17:56:59 +0000

    Update Feedback.jsx

--- src/components/Feedback/Feedback.jsx
+++ src/components/Feedback/Feedback.jsx
@@ -1,30 +1,25 @@
 import PropTypes from 'prop-types';
 import FeedbackButton from './FeedbackButton';
 import { List, Item, Wrapper } from './Feedback.styled';
-const { Component } = require('react');
 
-class Feedback extends Component {
-  render() {
-    const { options, buttonHandler } = this.props;
-
-    return (
-      <Wrapper>
-        <List>
-          {options.map(option => (
-            <Item key={option}>
-              <FeedbackButton
-                onClick={() => {
-                  buttonHandler(option);
-                }}
-                value={option}
-              />
-            </Item>
-          ))}
-        </List>
-      </Wrapper>
-    );
-  }
-}
+const Feedback = ({ options, buttonHandler }) => {
+  return (
+    <Wrapper>
+      <List>
+        {options.map(option => (
+          <Item key={option}>
+            <FeedbackButton
+              onClick={() => {
+                buttonHandler(option);
+              }}
+              value={option}
+            />
+          </Item>
+        ))}
+      </List>
+    </Wrapper>
+  );
+};
 
 export default Feedback;
 
